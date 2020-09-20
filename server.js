04f6065fb66f748bdcfd09329c8d590f1ab7d421
Nguyen, Tien <ntien@ntien.com>
2020-09-20 00:15:50 +0000

    api 5 done

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
-'use strict';
-
-var express = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
 
 // require and use "multer"...
+var multer = require("multer");
+var upload = multer({ dest: "uploads/" });
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.get("/", function (req, res) {
+	res.sendFile(process.cwd() + "/views/index.html");
+});
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.post("/api/fileanalyse", upload.single("uploaded_file"), function (
+	req,
+	res,
+	next
+) {
+	res.json({ name: req.originalName, type: req.mimetype, size: req.size });
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+	console.log("Node.js listening ...");
 });
